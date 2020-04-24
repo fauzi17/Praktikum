75c080fd5ce7dc8f8f9f425c9d2e9c6fe3ee7b3f
Praktikum <fauzi.martapura@gmail.com>
2020-04-24 14:51:24 +0700

    Praktikum

---
+++ app/src/main/java/com/e/praktikumc/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.e.praktikumc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
