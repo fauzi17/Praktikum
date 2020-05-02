6106b88d1ff6f03ff868337a1e74ea92206d8b99
Praktikum <fauzi.martapura@gmail.com>
2020-05-02 14:02:27 +0700

    Implementasi Hitung

---
+++ app/src/main/java/com/e/praktikumc/HitungActivity.kt
@@ -0,0 +1,12 @@
+package com.e.praktikumc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HitungActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hitung)
+    }
+}
